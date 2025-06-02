64dd47d6bdc6b3738a16fa59f439d86466d24c6b
Jiayue Yu <z5448095@ad.unsw.edu.au>
2025-06-03 08:29:28 +1000

    Add loading info to the guestLoginBtn. TODO: error handling.

--- app/ui/components/authentication/guestLoginBtn.tsx
+++ app/ui/components/authentication/guestLoginBtn.tsx
@@ -1,19 +1,26 @@
 "use client";
 
+import { useState } from "react";
 import { createGuestUser } from "@/lib/actions";
 
 export default function GuestLoginBtn() {
+  const [loading, setLoading] = useState(false);
+
   const handleGuest = async () => {
+    setLoading(true);
     await createGuestUser();
+    setLoading(false);
   };
+
   return (
-    <>
-      <button
-        onClick={handleGuest}
-        className="bg-[#FF495C] text-white rounded-md w-40 h-8"
-      >
-        Try it out as a guest
-      </button>
-    </>
+    <button
+      onClick={handleGuest}
+      disabled={loading}
+      className={`rounded-md w-40 h-8 ${
+        loading ? "bg-gray-400 cursor-not-allowed" : "bg-[#FF495C]"
+      } text-white`}
+    >
+      {loading ? "Logging in..." : "Try it out as a guest"}
+    </button>
   );
 }
