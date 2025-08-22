04325bd2e7b618fa9ce21651795f2ca2658b7748
Jennifer-JY <yujiayuejy@gmail.com>
2025-08-22 14:04:43 +1000

    Add suspense back in the register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import RegisterForm from "../ui/components/authentication/register";
 
 const Register = () => {
   return (
     <div className="flex justify-center h-screen items-center bg-[#c0c0c0]">
-      <RegisterForm />
+      <Suspense>
+        <RegisterForm />
+      </Suspense>
     </div>
   );
 };
